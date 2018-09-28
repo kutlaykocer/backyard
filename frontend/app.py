4aadbf7228939967d9236573af2f8c86a80364eb
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2018-09-28 14:51:49 +0200

    Make storage visible to frontend

--- frontend/app.py
+++ frontend/app.py
@@ -6,8 +6,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
+    message = ""
+    datadir = 'data'
+    if os.path.isdir(datadir):
+        message += "Folder exists!"
+        message += str(os.listdir(datadir))
+    else:
+        message += "Error: Folder doesn't exist!"
     provider = str(os.environ.get('PROVIDER', 'world'))
-    return 'Hello ' + provider + '!'
+    return 'Hello ' + provider + '!\n' + message
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
