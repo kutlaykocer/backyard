451d289ab8dd5410dc1ec59f3293b040eb94144a
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2018-09-28 10:41:40 +0200

    Add progress bars and dummy delay

--- backend/analysis.py
+++ backend/analysis.py
@@ -1,5 +1,7 @@
 import json
 import sys
+import time
+from tqdm import tqdm
 
 
 def perform_analysis(url):
@@ -7,11 +9,16 @@ def perform_analysis(url):
     outfilepath = 'data/result_{}.json'.format(url)
 
     print('Performing analysis ...')
+    # dummy analysis
     json_data = {}
     with open(filepath) as f:
         json_data = json.load(f)
     json_data["info"] = "Everything is analyzed!"
     result = json_data
+    # wait dummy time
+    for i in tqdm(range(0, 10)):
+        time.sleep(0.2)
+        print('- doing important analysis work, part {} ...'.format(i))
 
     print('Storing results in ' + filepath)
     with open(outfilepath, 'w') as outfile:
--- backend/worker.py
+++ backend/worker.py
@@ -1,13 +1,20 @@
 import json
 import datetime
 import sys
+import time
+from tqdm import tqdm
 
 
 def gather_data(url):
     filepath = 'data/data_{}.json'.format(url)
 
     print('Gathering data ...')
+    # gather dummy data
     result = {"time": f"{datetime.datetime.now():%Y-%m-%d %H:%M:%S}", "URL": url}
+    # wait dummy time
+    for i in tqdm(range(0, 10)):
+        time.sleep(0.2)
+        print('- finding important data, part {} ...'.format(i))
 
     print('Storing results in ' + filepath)
     with open(filepath, 'w') as outfile:
