87922fa652af1c273f6fca2f540a9e7d9a52dbc8
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2018-10-01 15:39:47 +0200

    Make backend a server

---
+++ backend/app.py
@@ -0,0 +1,24 @@
+import os
+
+import flask
+
+import master
+
+
+app = flask.Flask(__name__, template_folder='./')
+
+@app.route('/')
+def index():
+    return flask.render_template('index.html')
+
+
+@app.route('/request/', methods=['POST'])
+def request_result():
+    url = flask.request.form['url']
+    return flask.jsonify(master.backend_get(url))
+
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
