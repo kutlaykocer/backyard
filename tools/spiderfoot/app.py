8051357d027892c129ee50682b7bc21b558ee7d7
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2018-10-15 12:29:24 +0200

    Let script initiate scans

--- tools/spiderfoot/app.py
+++ tools/spiderfoot/app.py
@@ -1,9 +1,28 @@
+from __future__ import print_function
+
 import os
 
 
 if __name__ == '__main__':
-    print "Calling the spiderfoot server ..."
+    url = 'spiderfoot@gmail.com'
+    print("Calling the spiderfoot server to analyse {} ...".format(url))
+
+    # get environmentals
     _backend_addr = os.environ["SPIDERFOOT_PORT_5001_TCP_ADDR"]
     _backend_port = os.environ["SPIDERFOOT_PORT_5001_TCP_PORT"]
     _target = "http://{}:{}".format(_backend_addr, _backend_port)
-    os.system("python sfcli.py -s {}".format(_target))
+
+    # configure scans
+    _cmd_file = 'sf_cmd.txt'
+    modules = ['sfp_pwned']
+    with open(_cmd_file, 'w') as f:
+        print('start {} -m {}'.format(url, ','.join(modules)), file=f)
+
+    # configure output
+    _output_file = 'sf_output.txt'
+    _log_file = 'sf_log.txt'
+
+    # run it
+    _cmd = "python sfcli.py -s {} -e {} -o {} -l {}".format(_target, _cmd_file, _output_file, _log_file)
+    print("Executing: " + _cmd)
+    os.system(_cmd)
