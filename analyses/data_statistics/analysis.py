2ec9f9422fd697c92d620e6ab713dd39c8414ee1
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2018-10-26 09:38:24 +0200

    Move scan info to data_statistics analysis

--- analyses/data_statistics/analysis.py
+++ analyses/data_statistics/analysis.py
@@ -1,14 +1,30 @@
+import datetime
+import json
+import os
 import sys
 
 
 def run(data_dir):
 
-    print('Opening datafile in ' + data_dir)
+    result = {
+        "time": f"{datetime.datetime.now():%Y-%m-%d %H:%M:%S}",
+        }
 
-    # dummy data for now
-    result = {'number_of_host_IPs': 31337,
-              'number_of_emails': 31337,
-              }
+    print('Opening datafiles in ' + data_dir + ':')
+    _data_files = os.listdir(data_dir)
+    _data_files_info = []
+    for file in _data_files:
+        print('- ' + file)
+        full_path = os.path.join(data_dir, file)
+        size = os.path.getsize(full_path)
+        info = {'file_name': file, 'size_bytes': size}
+        _data_files_info.append(info)
+
+    result['number_of_data_files'] = len(_data_files)
+    result['data_files'] = _data_files_info
+
+    with open(os.path.join(data_dir, 'result.json')) as result_file:
+        result['scan_info'] = json.load(result_file)
 
     return result
 
