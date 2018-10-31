097a4f5cb714c0ee6d9264bb987ad788cff774b6
Cajus Pollmeier <pollmeier@gonicus.de>
2018-10-31 15:49:49 +0100

    Run docker to get a POC flow up and running

--- api/src/backyard/supervisor/pod.py
+++ api/src/backyard/supervisor/pod.py
@@ -1,4 +1,10 @@
+import logging
+import subprocess
+
+
 def run(image, a_id, domain):
     # TODO: Run docker image/kubernetes POD creation
-    
-    print('$ docker run --net=host -e DOMAIN="%s" -e ANALYZER="%s" %s' % (domain, a_id, image))
+    subprocess.Popen(["docker", "run", "--net=host", "-e",
+                      "DOMAIN=%s" % domain, "-e", "ANALYZER=\"%s\"" % a_id,
+                      image])
+    logging.info('docker run --net=host -e DOMAIN="%s" -e ANALYZER="%s" %s' % (domain, a_id, image))
