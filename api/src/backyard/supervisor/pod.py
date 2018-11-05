ddcdedbda802e6baaff515cd3c62ecf395f88133
Cajus Pollmeier <pollmeier@gonicus.de>
2018-11-05 11:46:31 +0100

    Updated docker run

--- api/src/backyard/supervisor/pod.py
+++ api/src/backyard/supervisor/pod.py
@@ -1,10 +1,15 @@
+import json
 import logging
 import subprocess
 
 
-def run(image, a_id, domain):
+def run(image, a_id, domain, paths=None):
     # TODO: Run docker image/kubernetes POD creation
+    res = ""
+    if not paths is None:
+        res = json.dumps(paths)
+
     subprocess.Popen(["docker", "run", "-v", "/tmp/data:/data/%s" % domain, "--net=host", "-e",
-                      "DOMAIN=%s" % domain, "-e", "ANALYZER=\"%s\"" % a_id,
+                      "DOMAIN=%s" % domain, "-e", "ANALYZER=%s" % a_id, "-e", "SCANS=%s" % res.replace('"','\\"'),
                       image])
-    logging.info('docker run -v /tmp/data:/data/%s --net=host -e DOMAIN="%s" -e ANALYZER="%s" %s' % (domain, domain, a_id, image))
+    logging.info('docker run -v /tmp/data:/data/%s --net=host -e DOMAIN="%s" -e ANALYZER="%s"  -e SCANS="%s" %s' % (domain, domain, a_id, res.replace('"','\\"'), image))
