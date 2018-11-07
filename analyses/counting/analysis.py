7869f9278b31579ba8501a42205faaaf9afbc7ab
Parekh <parek-mi@de001.itgr.net>
2018-11-07 11:17:29 +0100

    Count some results

--- analyses/counting/analysis.py
+++ analyses/counting/analysis.py
@@ -8,31 +8,55 @@ def run(data_dir):
 
     print('Opening datafile in ' + data_dir)
 
-    file_name = 'results.xml'
+    file_name = 'myresults.xml'
     full_file = os.path.abspath(os.path.join(data_dir, file_name))
 
+    parsed_data = ElementTree.parse(full_file)
 
-    dom = ElementTree.parse(full_file)
+    result_details = {'email' : {}, 'Host_name': {}, 'Virtual_Host_name': {}, 'TLD':{}, 'Shodan':{}}
+    result = {'n_emails':{}, 'n_hostnames':{},'n_virtual_hostnames':{}}
 
-    f = dom.findall('email')
+    # Emails
+    occurances = parsed_data.findall('email')
+
+    for occurance in occurances:
+        email_adr = occurance.text
+        result_details['email'][email_adr] = email_adr
+
+    result['n_emails'] = len(result['email'])
 
 
-    for e in f:
-        email_adr = e.text
-        print(email_adr)
-
-    h = dom.findall('host')
-
-    result = {}
+    # Hostnames - IP addresses
+    h = parsed_data.findall('host')
 
     for n in h:
         host_name = n.find('ip').text
         ip_adr = n.find('hostname').text
-        # description = n.find('description').text
-        # print(' * {} {} {} '.format(name, price, description))
-        result[host_name] = ip_adr
-    # print(result)
+        result_details['Host_name'][host_name] = ip_adr
+
+    result['n_hostnames'] = len(result['Host_name'])
+
+
+    # VirtualHosts - IP addresses
+    vh = parsed_data.findall('vhost')
+
+    for m in vh:
+        vhost_name = m.find('hostname').text
+        ip_adr_v = m.find('ip').text
+        result_details['Virtual_Host_name'][vhost_name] = ip_adr_v
+
+    result['n_virtual_hostnames'] = len(result['Virtual_Host_name'])
+
+    # TLD
+    t = parsed_data.findall('tld')
+
+    # Shodan
+    s = parsed_data.findall('shodan')
+
+    # print data in JSON format
     print(json.dumps(result, indent = 4))
+    print(json.dumps(result_details, indent = 4))
+
     return result
 
 
